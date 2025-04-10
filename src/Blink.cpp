d98291e9982ba67d04c530c32046d85b7b51053d
ilhanhusaeri <134351968+ilhanhusaeri@users.noreply.github.com>
2025-04-10 17:51:16 +0700

    RTOStest

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -1,17 +1,35 @@
 #include <Arduino.h>
-// Blink LED on ESP32
 
+
+// Task untuk nyalakan/matikan LED
+void LEDTask(void *pvParameters) {
+    while (1) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        vTaskDelay(500 / portTICK_PERIOD_MS); // 500ms ON
+        digitalWrite(LED_BUILTIN, LOW);
+        vTaskDelay(500 / portTICK_PERIOD_MS); // 500ms OFF
+    }
+}
+
+// Task untuk menghitung dari 1 sampai 10 di Serial Monitor
+void CounterTask(void *pvParameters) {
+    int count = 1;
+    while (1) {
+        Serial.println("Hitung: " + String(count));
+        count++;
+        if (count > 10) count = 1;
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 detik
+    }
+}
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); // Set pin LED sebagai output
-  Serial.begin(115200);
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    xTaskCreate(LEDTask, "LEDTask", 2048, NULL, 1, NULL);
+    xTaskCreate(CounterTask, "CounterTask", 2048, NULL, 1, NULL);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // Nyalakan LED
-  delay(1000);                      // Tunggu 1 detik
-  digitalWrite(LED_BUILTIN, LOW);   // Matikan LED
-  delay(1000);                      // Tunggu 1 detik
-  Serial.println("ok!");
-  delay(1000);
 }
+
