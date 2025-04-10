cb76174e2524d6012a116af1081bb54476f1d7aa
ilhanhusaeri <134351968+ilhanhusaeri@users.noreply.github.com>
2025-04-10 17:44:10 +0700

    newblink

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -1,30 +1,17 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-
 #include <Arduino.h>
+// Blink LED on ESP32
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-#ifndef LED_BUILTIN
-    #define LED_BUILTIN 2
-#endif
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); // Set pin LED sebagai output
+  Serial.begin(115200);
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  delay(1000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // Nyalakan LED
+  delay(1000);                      // Tunggu 1 detik
+  digitalWrite(LED_BUILTIN, LOW);   // Matikan LED
+  delay(1000);                      // Tunggu 1 detik
+  Serial.println("ok!");
   delay(1000);
 }
